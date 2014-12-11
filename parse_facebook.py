6410d1ddd5d2234f43ccfcb81def6c41c41ad9e9
Mike Perrone <mike.j.perrone@gmail.com>
2014-12-11 01:41:43 +0100

    parse the wall for status updates

--- parse_facebook.py
+++ parse_facebook.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # usage: `python path/to/facebook/output`
 
-# here's the structure of the facebook output:
+
+import sys
+import xml.etree.ElementTree as ElementTree
+
+
+# here's the structure of the facebook messages output:
 # html
 #   body
 #     div class contents
@@ -16,11 +21,6 @@
 #           p <text>
 #           div class message ...
 #           p <text>
-
-import sys
-import xml.etree.ElementTree as ElementTree
-
-
 def parse_messages(filename):
     doc = ElementTree.iterparse(
         filename,
@@ -39,6 +39,47 @@ def parse_messages(filename):
             if elem.tag == 'p':
                 if user in ('Mike Perrone', 'Michael Perrone'):
                     yield elem.text
+
+
+# here's the structure of the facebook wall output:
+# html
+#   body
+#     div class contents
+#       div
+#         div class meta
+#           date...
+#         Mike Perrone updated his status
+#         div class comment
+#           <status update>
+#         p
+#         p
+#         div class meta
+#           date...
+#       ...
+#
+# let's just look for status updates for now...
+def parse_wall(filename):
+    doc = ElementTree.iterparse(
+        filename,
+        ('start', 'end'),
+        parser=ElementTree.XMLParser(
+            target=ElementTree.TreeBuilder(),
+            encoding='utf-8'
+        )
+    )
+    status = False
+
+    for event, elem in doc:
+        if event == 'start':
+            if status and elem.attrib.get('class') == 'comment':
+                yield elem.text
+            if elem.tag == 'div':
+                status = elem.tail and 'Mike Perrone updated his' in elem.tail
+
+
+data = parse_wall(sys.argv[1] + "html/wall.htm")
+for message in data:
+    print message
 
 data = parse_messages(sys.argv[1] + "html/messages.htm")
 for message in data:
