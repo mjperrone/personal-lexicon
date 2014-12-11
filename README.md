cdc86ed0319da6b87ea500bbbcd2c2be1700d58c
Mike Perrone <mike.j.perrone@gmail.com>
2014-12-11 02:51:08 +0100

    add parse fb photo comments

--- README.md
+++ README.md
@@ -10,7 +10,6 @@ you download all your data](https://www.facebook.com/help/131112897028467/), so
 I don't have to do something janky and parse this data off of their UI.
 
 ###Todo
-1. parse Facebook photo comment data
 1. parse gmail outbox
 1. parse google hangouts
 1. actually do some analysis with the parsed text...
--- parse_facebook.py
+++ parse_facebook.py
@@ -3,6 +3,7 @@
 
 import sys
 import xml.etree.ElementTree as ElementTree
+import glob
 
 
 # here's the structure of the facebook messages output:
@@ -79,6 +80,40 @@ def parse_wall(filename):
                      'Mike Perrone added a new photo to the album']))
 
 
+# here's the structure of the facebook photos output:
+# html
+#   body
+#     div class contents
+#       div class block
+#         div
+#           div class meta
+#             <date photo posted>
+#           div class comment
+#             span class user
+#               <name of commenter>
+#             <the comment>
+#             span class meta
+#               <date of comment>
+#           div class comment
+#           ... and so on for more comments on thie photo ...
+#       div class block
+#         ... and  so on for more pictures in this album ...
+def parse_photo_comments(filename):
+    doc = ElementTree.iterparse(
+        filename,
+        ('start', 'end'),
+        parser=ElementTree.XMLParser(
+            target=ElementTree.TreeBuilder(),
+            encoding='utf-8'
+        )
+    )
+
+    for event, elem in doc:
+        if event == 'start':
+            if elem.attrib.get('class') == 'user' and elem.text == 'Mike Perrone':
+                yield elem.tail
+
+
 data = parse_wall(sys.argv[1] + "html/wall.htm")
 for message in data:
     print message
@@ -86,3 +121,8 @@ for message in data:
 data = parse_messages(sys.argv[1] + "html/messages.htm")
 for message in data:
     print message
+
+for photo in glob.glob(sys.argv[1] + "photos/*/index.htm"):
+    data = parse_photo_comments(photo)
+    for message in data:
+        print message
